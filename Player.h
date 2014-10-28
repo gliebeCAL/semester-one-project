af5d2503ac19e8b4ac347fcd38fdf410777faa32
chang5CAL <chang5@calcharter.org>
2014-10-28 09:49:36 -0700

    Create Player.h
    Player class, is simple, will need upgrades when later systems are implemented.

---
+++ Player.h
@@ -0,0 +1,27 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <SFML/Graphics.hpp>
+
+class Player
+{
+    public:
+        Player(int xSize,int ySize);
+        void Spawn(int spawnX,int spawnY);
+        //Should just be X's and Y's
+
+        void MoveUp();
+        void MoveDown();
+        void MoveLeft();
+        void MoveRight();
+        //These move it. Should be
+
+        sf::RectangleShape sprite;
+        int posX,posY;
+
+
+        //I'll probably need more things, like stats and inventory. But for now, this'll do.
+    protected:
+    private:
+};
+
+#endif // PLAYER_H

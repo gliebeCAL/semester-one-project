a8cdf1f7135f39981f422f35be6a359277f96983
chang5CAL <chang5@calcharter.org>
2014-12-01 10:44:46 -0800

    Update Melee_Enemy.h

--- Melee_Enemy.h
+++ Melee_Enemy.h
@@ -9,20 +9,29 @@
 class Melee_Enemy : public Enemy
 {
     public:
-        Melee_Enemy(int difficulty,int spawnX, int spawnY,int aiType, int eType, int intelligence, int xCap, int yCap);
-        void Create(int difficulty,int spawnX, int spawnY,int aiType, int eType, int intelligence, int xCap, int yCap);
+        Melee_Enemy(int difficulty,int spawnX, int spawnY,int aiType, int eType, int intelligence, int levelxCap, int levelyCap,int dun[100][100]);
+        void Create(int difficulty,int spawnX, int spawnY,int aiType, int eType, int intelligence, int levelxCap, int levelyCap,int dun[100][100]);
         //Should get the constructor from it's parent.
-        void AIAction(/*int level[],bool levelOccupied[],*/int xCap,int yCap,int playerposX, int playerposY);
+        void AIAction(bool levelOccupied[100][100],int xCap,int yCap,int playerposX, int playerposY,int level[100][100]);
         void GenerateSprite();
         sf::Sprite sprite;
 
-        void MoveUp();
-        void MoveDown(int yCap);
-        void MoveLeft();
-        void MoveRight(int xCap);
+        void MoveUp(bool level_occupied[100][100],int level[100][100]);
+        void MoveDown(int yCap,bool level_occupied[100][100],int level[100][100]);
+        void MoveLeft(bool level_occupied[100][100],int level[100][100]);
+        void MoveRight(int xCap,bool level_occupied[100][100],int level[100][100]);
+
+        int Attack(int pHealth);
+
+        int health;
+
+        bool enemyCanAttack = false;
+        bool enemyDeleted = false;
+        bool enemyPosWipe = false;
     protected:
     private:
         sf::Texture texture;
+        int attackpwr;
 };
 
 #endif // MELEE_ENEMY_H

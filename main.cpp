408d86d660cd2be978eb4ba19e4e923608b87246
chang5CAL <chang5@calcharter.org>
2014-12-01 10:42:12 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@
 
 /*
 Note to self:
-Melee enemy is prototypically done (Need to implement stats, hitting, staying in the boundary, but it moves and
-                                    follows you.)
+Combat is implemented, need to work on multiple enemies, and make them spawn in the level.
+
+Learn pointers. Do it.
 */
 
 int main()
@@ -19,14 +20,18 @@ int main()
     //Used to see if the player has been spawned yet.
     bool playerMoved = false;
 
+    int melee_Enemies = 1;
+
     DunGen dungeon(10,10);
     Player pChar(50,50);
-    Melee_Enemy mEnemy(1,0,0,1,1,1,10,10);
+
+
+    Melee_Enemy mEnemy(1,0,0,1,1,1,10,10,dungeon.level);
     mEnemy.GenerateSprite();
 
 
     sf::View camera;
-    camera.setSize(800.f,600.f);
+    camera.setSize(800,600);
     camera.setCenter(pChar.posX,pChar.posY);
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "Dirt and Dungeons");
@@ -34,13 +39,15 @@ int main()
     int levelSize = 0;
 
 
-    //This should go in the loop.
+    dungeon.levelOccupied[pChar.posX][pChar.posY] = true;
+    dungeon.levelOccupied[mEnemy.posX][mEnemy.posY] = true;
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+
             if (event.type == sf::Event::Closed)
             {
                 window.close();
@@ -56,32 +63,77 @@ int main()
                 {
                     if (dungeon.level[pChar.posX + 1][pChar.posY] > 0 and pChar.posX < dungeon.levelXCap)
                     {
-                        pChar.MoveRight();
-                        playerMoved = true;
+                        if (dungeon.levelOccupied[pChar.posX+1][pChar.posY] == false)
+                        {
+                            dungeon.levelOccupied[pChar.posX][pChar.posY] = false;
+                            pChar.MoveRight();
+                            dungeon.levelOccupied[pChar.posX][pChar.posY] = true;
+                            playerMoved = true;
+                        }
+                        else
+                        {
+                            mEnemy.health = pChar.Attack(mEnemy.health);
+                            playerMoved = true;
+                            std::cout << "Enemy Health: " << mEnemy.health << std::endl;
+                        }
                     }
                 }
                 if (event.key.code == sf::Keyboard::A)
                 {
                     if (dungeon.level[pChar.posX - 1][pChar.posY] > 0 and pChar.posX > 0)
                     {
-                        pChar.MoveLeft();
-                        playerMoved = true;
+                        if (dungeon.levelOccupied[pChar.posX-1][pChar.posY] == false)
+                        {
+                            dungeon.levelOccupied[pChar.posX][pChar.posY] = false;
+                            pChar.MoveLeft();
+                            dungeon.levelOccupied[pChar.posX][pChar.posY] = true;
+                            playerMoved = true;
+                        }
+                        else
+                        {
+                            mEnemy.health = pChar.Attack(mEnemy.health);
+                            playerMoved = true;
+                            std::cout << "Enemy Health: " << mEnemy.health << std::endl;
+                        }
                     }
                 }
+
                 if (event.key.code == sf::Keyboard::S)
                 {
                     if (dungeon.level[pChar.posX][pChar.posY + 1] > 0 and pChar.posY < dungeon.levelXCap)
                     {
-                        pChar.MoveDown();
-                        playerMoved = true;
+                        if (dungeon.levelOccupied[pChar.posX][pChar.posY+1] == false)
+                        {
+                            dungeon.levelOccupied[pChar.posX][pChar.posY] = false;
+                            pChar.MoveDown();
+                            dungeon.levelOccupied[pChar.posX][pChar.posY] = true;
+                            playerMoved = true;
+                        }
+                        else
+                        {
+                            mEnemy.health = pChar.Attack(mEnemy.health);
+                            playerMoved = true;
+                            std::cout << "Enemy Health: " << mEnemy.health << std::endl;
+                        }
                     }
                 }
                 if (event.key.code == sf::Keyboard::W)
                 {
                     if (dungeon.level[pChar.posX][pChar.posY - 1] > 0 and pChar.posY > 0)
                     {
-                        pChar.MoveUp();
-                        playerMoved = true;
+                        if (dungeon.levelOccupied[pChar.posX][pChar.posY-1] == false)
+                        {
+                            dungeon.levelOccupied[pChar.posX][pChar.posY] = false;
+                            pChar.MoveUp();
+                            dungeon.levelOccupied[pChar.posX][pChar.posY] = true;
+                            playerMoved = true;
+                        }
+                        else
+                        {
+                            mEnemy.health = pChar.Attack(mEnemy.health);
+                            playerMoved = true;
+                            std::cout << "Enemy Health: " << mEnemy.health << std::endl;
+                        }
                     }
                 }
                 if (event.key.code == sf::Keyboard::Period)
@@ -112,9 +164,39 @@ int main()
 
         if (playerMoved == true)
         {
-            mEnemy.AIAction(10,10,pChar.posX,pChar.posY);
-            playerMoved = false;
+
+            if (mEnemy.enemyDeleted == true)
+            {
+                //Delete the enemy's positions.
+                dungeon.levelOccupied[mEnemy.posX][mEnemy.posY] = false;
+            }
+            else
+            {
+                dungeon.levelOccupied[mEnemy.posX][mEnemy.posY] = false;
+                mEnemy.AIAction(dungeon.levelOccupied,10,10,pChar.posX,pChar.posY,dungeon.level);
+                dungeon.levelOccupied[mEnemy.posX][mEnemy.posY] = true;
+                playerMoved = false;
+
+                if (mEnemy.enemyCanAttack == true)
+                {
+                    pChar.health = mEnemy.Attack(pChar.health);
+                    std::cout << "Player Health: " << pChar.health << std::endl;
+                    mEnemy.enemyCanAttack = false;
+                }
+
+                if (mEnemy.health <= 0)
+                {
+                    dungeon.levelOccupied[mEnemy.posX][mEnemy.posY] = false;
+                }
+            }
         }
+
+        /*for(int m=0;m<meleeEnemies;m++)
+        {
+            dungeon.levelOccupied[melee[m].posX][melee[m].posY] = true;
+        }*/
+
+        //So what these do is set what opts are occupied at the end of every loop.
 
 
 

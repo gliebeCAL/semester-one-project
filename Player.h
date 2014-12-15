79f958333da7b9b7b7307524ba17df8e66e9bd95
chang5CAL <chang5@calcharter.org>
2014-12-14 16:02:16 -0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -13,16 +13,22 @@ class Player
         void MoveDown();
         void MoveLeft();
         void MoveRight();
-        //These move it. Should be
+        //These move it.
+
+        int Attack(int eHealth);
+
+        void AddToInventory();
+        //Make an item class, so that I can make an inventory array.
 
         sf::Sprite sprite;
         int posX,posY;
 
-
+        int health;
         //I'll probably need more things, like stats and inventory. But for now, this'll do.
     protected:
     private:
         sf::Texture texture;
+        int attackpwr;
 };
 
 #endif // PLAYER_H
